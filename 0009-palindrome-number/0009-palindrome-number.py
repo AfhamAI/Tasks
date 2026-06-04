d7301d2e57e0f88e6f11cc13649e05bd7edcd2e9
Afham <afhamtab@gmail.com>
2026-06-04 08:06:00 +0530

    Time: 8 ms (71.11%), Space: 12.5 MB (18.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x = str(x)
+        rev = x[::-1]
+        
+        if x == rev:
+            return True
+        else:
+            return False
+        
