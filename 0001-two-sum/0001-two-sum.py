8a101e7f699b36edc0b8af691d12203f40e4421d
Afham <afhamtab@gmail.com>
2026-06-05 14:09:01 +0530

    Time: 0 ms (100%), Space: 13.1 MB (52.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,12 @@ class Solution(object):
     def twoSum(self, nums, target):
         n = len(nums)
         hashMap = {}
+
         for i in range(0,n):
             remaining = target - nums[i]
+            
             if remaining in hashMap:
-                return [hashMap[remaining] , i ]
+                return [hashMap[remaining] , i]
             else:
                 hashMap[nums[i]] = i
+                
