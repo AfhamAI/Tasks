7161f01cc0af8573cc7c1b81b7e7545a8dc17c1e
Afham <afhamtab@gmail.com>
2026-06-05 08:23:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.35%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -1,12 +1,21 @@
 class Solution(object):
     def smallestEvenMultiple(self, n):
+        if n%2 == 0:
+            return n
+        else:
+            return n*2
 
-        ans = None
-        li = []
-        for i in range(1,11):
-            ans = n*i
-            li.append(ans)
-        for i in li:
-            if i%2==0:
-                return i
-                break
+
+
+
+        # ans = None
+        # li = []
+        # for i in range(1,11):
+        #     ans = n*i
+        #     li.append(ans)
+        # for i in li:
+        #     if i%2==0:
+        #         return i
+
+
+                
