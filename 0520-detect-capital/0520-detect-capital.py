dba1198d87ca2658686c6cd6e8340db799258d74
Afham <afhamtab@gmail.com>
2026-06-09 14:49:29 +0530

    Time: 1 ms (13.47%), Space: 12.3 MB (51.61%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        c = 0
+        d = 1
+        if len(word) == 1:
+            return True
+        if word == word.lower():
+            return True
+        for i in range(1,len(word)):
+            if word[0] == word[0].upper() and word[i] == word[i].lower():
+                d += 1
+            else:
+                break
+
+        for i in word:
+            if i == i.upper():
+                c += 1
+
+        if c == len(word):
+            return True
+        elif d == len(word):
+            return True 
+        else:
+            return False
+
+
+            
