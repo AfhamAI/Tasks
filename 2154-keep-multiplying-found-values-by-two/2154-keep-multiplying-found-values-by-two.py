fd0ee79b60fee3a3217afd7bb16a0b771c49552f
Afham <afhamtab@gmail.com>
2026-06-10 12:11:00 +0530

    Time: 0 ms (100%), Space: 12.5 MB (63.05%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        while original in nums:
+            for i in nums:
+                if i == original:
+                    original = original*2
+                    break
+            else:
+                return original
+        return original
+        
+
+        
+            
+        
